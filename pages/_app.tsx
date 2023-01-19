76ff3a046373d3302ab4c521faf0a97b976453d0
eds2002 <es23jr@gmail.com>
2023-01-19 04:54:03 -0500

    install hotjar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Roboto } from '@next/font/google'
+import { hotjar } from 'react-hotjar'
+import { useEffect } from 'react'
 
 const roboto = Roboto({
   subsets: ['cyrillic-ext'],
@@ -8,6 +10,23 @@ const roboto = Roboto({
   weight: ['300', '400', '500', '700', '900'],
 })
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    hotjar.initialize(
+      process.env.NEXT_PUBLIC_HJID as any,
+      process.env.NEXT_PUBLIC_HSJV as any
+    )
+
+    hotjar.identify('USER_ID', { userProperty: 'value' })
+
+    hotjar.event('button-click')
+
+    // Update SPA state
+    hotjar.stateChange('/my/page')
+
+    if (hotjar.initialized()) {
+      hotjar.identify('USER_ID', { userProperty: 'value' })
+    }
+  }, [])
   return (
     <main className={roboto.className}>
       <Component {...pageProps} />
